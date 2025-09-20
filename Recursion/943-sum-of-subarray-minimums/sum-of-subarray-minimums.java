47d00f15383195c0e1a8cb84aae1e35adc0e4664
pitamnish36 <pitamkumar36@gmail.com>
2025-09-21 02:51:01 +0530

    Time: 41 ms (82.94%) | Memory: 48.6 MB (96.14%) - LeetSync

--- Recursion/943-sum-of-subarray-minimums/sum-of-subarray-minimums.java
+++ Recursion/943-sum-of-subarray-minimums/sum-of-subarray-minimums.java
@@ -1,40 +1,22 @@
 class Solution {
     public int sumSubarrayMins(int[] arr) {
-        long modulo=100_000_0007;
-        int n=arr.length;
         Stack<Integer> stack=new Stack<>();
-        int left[]=new int[n];
-        int right[]=new int[n];
-
-        for(int i=0;i<n;i++){
-            while(!stack.isEmpty() && arr[stack.peek()]>arr[i]){
-                stack.pop();
+        long modulo=100_000_0007;
+        stack.push(-1);
+        long ans=0;
+        for(int i=0;i<=arr.length;i++){
+            int curVal=(i==arr.length)?0:arr[i];
+            while(stack.peek()!=-1 && curVal<=arr[stack.peek()]){
+                int p=stack.pop();
+                int j=stack.peek();
+                int left=p-j;
+                int right=i-p;
+                long sum=(long)(left*right*(long)arr[p])%modulo;
+                ans+=sum;
+                ans=ans%modulo;
             }
-            if(stack.isEmpty())
-            left[i]=-1;
-            else
-            left[i]=stack.peek();
             stack.push(i);
         }
-
-        stack.clear();
-
-        for(int i=n-1;i>=0;i--){
-            while(!stack.isEmpty() && arr[stack.peek()]>=arr[i]){
-                stack.pop();
-            }
-            if(stack.isEmpty())
-            right[i]=n;
-            else
-            right[i]=stack.peek();
-            stack.push(i);
-        }
-        long min=0;
-        for(int i=0;i<n;i++){
-            long subarr=((i-left[i])*(right[i]-i))%modulo;
-            long sum=(arr[i]*subarr)%modulo;
-            min=(min+sum)%modulo;
-        }
-        return (int)min;
+        return (int)ans;
     }
 }
