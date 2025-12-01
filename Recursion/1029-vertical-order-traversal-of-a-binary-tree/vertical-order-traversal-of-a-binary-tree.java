a1801e3a0bf31d4c26952b97f76b49db48ed2763
pitamnish36 <pitamkumar36@gmail.com>
2025-12-02 00:21:41 +0530

    Time: 3 ms (85.31%) | Memory: 44 MB (26.94%) - LeetSync

---
+++ Recursion/1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.java
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> verticalTraversal(TreeNode root) {
+        List<int[]> positions=new ArrayList<>();
+        collectNode(root,positions,0,0);
+        positions.sort((a,b)->{
+            if(a[0]!=b[0]) return Integer.compare(a[0],b[0]);
+            if(a[1]!=b[1]) return Integer.compare(a[1],b[1]);
+            return Integer.compare(a[2],b[2]);
+        });
+
+        List<List<Integer>> result=new ArrayList<>();
+        int prevCol=Integer.MIN_VALUE;
+
+        for(int[] position:positions){
+            int col=position[0],val=position[1];
+            if(col!=prevCol){
+                result.add(new ArrayList<>());
+                prevCol=col;
+            }
+            result.get(result.size()-1).add(position[2]);
+        }
+        return result;
+    }
+    public void collectNode(TreeNode root,List<int[]> positions,int row,int col){
+        if(root==null){
+            return;
+        }
+        collectNode(root.left,positions,row+1,col-1);
+        positions.add(new int[]{col,row,root.val});
+        collectNode(root.right,positions,row+1,col+1);
+    }
+}
