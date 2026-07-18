f1c6dacf14eb6c56a4a3ef9101820571642c52e6
pitamnish36 <pitamkumar36@gmail.com>
2026-07-18 17:33:22 +0530

    Time: 76 ms (34.60%) | Memory: 106.7 MB (61.17%) - LeetSync

---
+++ Recursion/739-daily-temperatures/daily-temperatures.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int n=temperatures.length;
+        int []answer=new int[n];
+        Stack<Integer> stack=new Stack<>();
+        for(int i=n-1;i>=0;i--){
+            while(!stack.isEmpty() && temperatures[stack.peek()]<=temperatures[i]){
+                stack.pop();
+            }
+            answer[i]=stack.isEmpty()?0:stack.peek()-i;
+            stack.push(i);
+        }
+        return  answer;
+    }
+}
